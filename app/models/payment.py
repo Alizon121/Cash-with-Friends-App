51ef47b58fb8706b9023818c851733495c4581db
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-01-26 14:10:11 -0600

    Added relationships between payments and users/expenses in payments model, as well as between payments and users in users model

--- app/models/payment.py
+++ app/models/payment.py
@@ -4,11 +4,13 @@ The Payment model represents individual payments made within the app
 This model is used to create and manage payment records within the application's database
 Uses the schema specified by SCHEMA .env variable
 Converts Payment instance into a dictionary for easy JSON serialization
+Defines relationships between payments & foreign keys for easy querying
 """
 
 
 from datetime import datetime, timezone
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from .db import db, environment, SCHEMA
 
 
@@ -28,6 +30,13 @@ class Payment(db.Model):
     paid_at = db.Column(db.Datetime, default=datetime.now(timezone.utc))
     created_at = db.Column(db.Datetime, default=datetime.now(timezone.utc))
     updated_at = db.Column(db.Datetime, default=datetime.now(timezone.utc))
+
+    # relationships defined for easier querying
+    # here we link each payment to an instance of 'expense' and an instance of 'user'
+    # so that we can direclty access a the expense or user related to a given payment
+    # i.e. payment.expense, payment.payer
+    expense = relationship('Expense', back_populates='payments')
+    payer = relationship('User', back_populates='payments')
 
     def to_dict(self):
         """
