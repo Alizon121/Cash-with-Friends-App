4490edbb4a291a11726edd8b4b43ecb9c32382cc
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-01-28 14:45:00 -0600

    Added first draft of Get All Payments for an Expense route in payment_routes.py

--- app/api/payment_routes.py
+++ app/api/payment_routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, redirect
+from flask import Blueprint, jsonify, redirect
 from flask_login import login_required, current_user
 from app.models import User
 from app.models.payment import Payment
+from app.models.expenses import Expense
 
 payment_routes = Blueprint('payments', __name__)
 
@@ -12,8 +13,44 @@ payment_routes = Blueprint('payments', __name__)
 def expense_payments(id):
     """
     Find all payments related to the specified expense
-    Query for every payment with the expenseId provided via route parameter (payments.id.expense_id)
+    Query for every payment using the expenseId provided via route parameter (payments.id.expense_id)
     """
+
+    # Query to find the expense
+    expense = Expense.query.get(id)
+    if not expense:
+        return jsonify({"Message": "Expense couldn't be found"}), 404
+
+    # Query to find all payments for the expense
+    payments = Payment.query.filter_by(expense_id=id).all()
+
+    # Format payments data into JSON response
+    payments_data = []
+
+    for payment in payments:
+        payment_data = {
+            "id": payment.id,
+            "expenseId": {
+                "description": expense.description,
+                "amount": expense.amount,
+                "settled": expense.settled,
+                "created_by": expense.created_by,
+                # use .isoformat() for datetime objects to stay consistent?
+                "created_at": expense.created_at.isoformat(),
+            },
+            "userId": {
+                "firstName": payment.payer.first_name,
+                "lastName": payment.payer.last_name,
+                "email": payment.payer.email,
+                "username": payment.payer.username,
+            },
+            "amount": payment.amount,
+            "paidAt": payment.paid_at.isoformat()
+        }
+
+        payments_data.append(payment_data)
+
+    return jsonify({"Payments": payments_data}), 200
 
 # ! Add a Payment to an Expense
 
