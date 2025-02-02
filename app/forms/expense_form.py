448fce777695c56225ac5f3ccc4a7505feb7b50f
Andrew <andrew.lizon12@gmail.com>
2025-02-02 07:23:43 -0800

    Refactoring expense routes and form to include validations

--- app/forms/expense_form.py
+++ app/forms/expense_form.py
@@ -6,9 +6,9 @@ from app.models import User
 
 class ExpenseForm(FlaskForm):
     participants=SelectMultipleField("Participants", choices=[], coerce=str)
-    description=StringField("Description", validators=[DataRequired(), Length(max=255, message="Description must not exceed 255 characters")])
-    amount=DecimalField("Amount", validators=[DataRequired()])
-    date=StringField("Date", validators=[DataRequired(), Regexp(r"^(0[1-9]|1[0-2])/(0[1-9]|1[0-9]|2[0-9]|3[01])/(\d{4})$", message="Date must be in the format mm/dd/yyyy")])
+    description=StringField("Description", validators=[DataRequired(message="Description is required"), Length(max=255, message="Description must not exceed 255 characters")])
+    amount=DecimalField("Amount", validators=[DataRequired(message="Amount is required")])
+    date=StringField("Date", validators=[DataRequired(message="Date is required"), Regexp(r"^(0[1-9]|1[0-2])/(0[1-9]|1[0-9]|2[0-9]|3[01])/(\d{4})$", message="Date must be in the format mm/dd/yyyy")])
     submit=SubmitField("Submit")
     # comment=StringField("Comment", validators=[DataRequired()])
     # Do we need to add a comment field? We do not need a comment field (01/31/2025)
