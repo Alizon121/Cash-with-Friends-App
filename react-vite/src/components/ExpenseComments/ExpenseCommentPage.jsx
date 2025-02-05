863bd429568785102f6ffb110b450ee17b7c210a
Andrew <andrew.lizon12@gmail.com>
2025-02-05 12:41:14 -0800

    Refactored the Comments components

---
+++ react-vite/src/components/ExpenseComments/ExpenseCommentPage.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux"
+
+function ExpenseCommentPage() {
+    const comments = useSelector(state => state.comments)
+
+    
+}
+
+export default ExpenseCommentPage
