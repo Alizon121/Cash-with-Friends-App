914e9fef479bfc7129d28479130a693d8dc18727
Johnny R Paredez III <jparedeziii@Mac.lan>
2025-02-06 11:34:44 -0600

    remove friend request modal done and added index.js to redux for cleaner imports

---
+++ react-vite/src/redux/index.js
@@ -0,0 +1,25 @@
+// react-vite/src/redux/index.js
+
+export * from "./csrf"
+export { default as configureStore } from "./store";
+
+export * as commentActions from "./comment";
+export { default as commentsReducer } from "./comment";
+
+export * as sessionActions from "./session";
+export { default as sessionReducer } from "./session";
+
+export * as expenseActions from "./expense";
+export { default as expenseReducer } from "./expense";
+
+// these aren't built yet but I assume that's what we will call them?
+export * as friendActions from "./friends"; 
+export { default as friendsReducer } from "./friends";
+
+export * as paymentActions from "./payments";
+export { default as paymentsReducer } from "./payments";
+
+// not entirely sure we need this one:
+export * as userActions from "./users";
+export { default as usersReducer } from "./users";
+
