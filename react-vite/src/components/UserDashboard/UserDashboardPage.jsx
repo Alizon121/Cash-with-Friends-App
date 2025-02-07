e700b04ce6a063134e7e90b0585b60b8cffafcb5
Andrew <andrew.lizon12@gmail.com>
2025-02-07 09:21:03 -0800

    Working on UserDashboard component

--- react-vite/src/components/UserDashboard/UserDashboardPage.jsx
+++ react-vite/src/components/UserDashboard/UserDashboardPage.jsx
@@ -1,9 +1,19 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react"
 import OpenModalMenuItem from "../Navigation/OpenModalMenuItem"
 import CreateExpenseModal from "../CreateExpenseModal/CreateExpenseModal"
+import AddFriendModal from "../AddFriendModal/AddFriendModal"
 
 function UserDashboardPage() {
     const user = useSelector(state => state.session)
+    const dispatch = useDispatch()
+
+    // useEffect(() => {
+        // Add the thunk actions for amount user owes and amount user is owed
+            // We may need to modify the backend to have the route diaply the total amounts
+    //     dispatch()
+    // })
+    
     return (
     <>
         <div>
@@ -12,9 +22,17 @@ function UserDashboardPage() {
                 <OpenModalMenuItem
                     modalComponent={<CreateExpenseModal />}
                     itemText={"Create"}
-
                 />
             </button>
+            <button>
+                <OpenModalMenuItem
+                    modalComponent={<AddFriendModal/>}
+                    itemText={"Add Friend"}
+                />
+            </button>
+            <div>
+                <span></span>
+            </div>
         </div>
         
     </>
--- react-vite/src/redux/expense.js
+++ react-vite/src/redux/expense.js
@@ -1,16 +1,26 @@
 import { csrfFetch } from "./csrf";
 
-// Make an action for settling/updating an expense
+////////////////////////////// Action Creators///////////////////////
+
+// Make an action for settling/updating an expense -ASL
 const SETTLE_EXPENSE = "SETTLE_EXPENSE"
-// Make an action creator for settling/updating an expense
 const settle = (expense) => {
     type = SETTLE_EXPENSE,
     payload = expense
 }
 
-// Make a thunk action that will settle an expense
+// Make an action creater for creating an expense -ASL
+const CREATE_EXPENSE = "CREATE_EXPENSE"
+const create = (expense) => {
+    type = CREATE_EXPENSE
+    payload = expense
+}
+
+////////////////////// Thunk Actions ////////////////////////////
+
+// Make a thunk action that will settle an expense -ASL
 export const settleExpenseThunk = (expenseId) => async dispatch => {
-    const response = await csrfFetch(`expenses/${expenseId}`, {
+    const response = await csrfFetch(`/api/expenses/${expenseId}`, {
         method: 'POST',
         body: JSON.stringify(payload)
     })
@@ -22,6 +32,25 @@ export const settleExpenseThunk = (expenseId) => async dispatch => {
     }
 }
 
+// Thunk action for creating an expense
+export const createExpenseThunk = (payload) => async dispatch => {
+    const response = await csrfFetch("/api/expenses/", {
+        method: 'POST',
+        body: JSON.stringify(payload)
+    })
+
+    if (response.ok) {
+        const result = await response.json()
+        dispatch(create) 
+        return result
+    } else {
+        const errorResult = await response.json()
+        console.error(errorResult)
+        throw new Error("Failed to create expense")
+    }
+}
+
+// Make the expense reducer -ASL
 const expenseReducer = (state={}, action) => {
     switch(action.type) {
         case SETTLE_EXPENSE: {
@@ -29,6 +58,14 @@ const expenseReducer = (state={}, action) => {
             return {
                 ...state,
                 [settledExpense.id]:settledExpense
+            }
+        }
+        case CREATE_EXPENSE: {
+            return {
+                ...state,
+                [action.payload.id]: {
+                    ...action.payload
+                }
             }
         }
         default:
