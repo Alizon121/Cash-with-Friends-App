60011095ffbc02df02fc44a8686a625433606dea
Andrew <andrew.lizon12@gmail.com>
2025-02-12 11:57:07 -0800

    Provided styling to dashboard

--- react-vite/src/components/UserDashboard/UserDashboardPage.jsx
+++ react-vite/src/components/UserDashboard/UserDashboardPage.jsx
@@ -7,6 +7,7 @@ import { loadAllUserExpensesThunk } from "../../redux/expense"
 import SettleFormModal from "../SettleFormModal/SettleFormModal"
 import { useNavigate } from "react-router-dom"
 import { getUsers } from "../../redux/users"
+import "./UserDashboard.css"
 
 function UserDashboardPage() {
     const user = useSelector(state => state.session)
@@ -38,76 +39,89 @@ function UserDashboardPage() {
 
     return (
     <>
+    {/* Dashboard Header */}
         <div>
-            <h2>{user.user.first_name}'s' Dashboard</h2>
-            <button>
-                <OpenModalMenuItem
-                    modalComponent={<CreateExpenseModal />}
-                    itemText={"Create"}
-                    
-                />
-            </button>
-            <button>
-                <OpenModalMenuItem
-                    modalComponent={<AddFriendModal/>}
-                    itemText={"Add Friend"}
-                />
-            </button>
-            <div>
+            <div className="dashboard_header_container">
+                <h2>{user.user.first_name}'s' Dashboard</h2>
+                <div className="dashboard_create_add_buttons_container">
+                    <button id="dashboard_create_expense_button">
+                        <OpenModalMenuItem
+                            modalComponent={<CreateExpenseModal />}
+                            itemText={"Create"}
+                            
+                        />
+                    </button>
+                    <button id="dashboard_add_friend_button">
+                        <OpenModalMenuItem
+                            modalComponent={<AddFriendModal/>}
+                            itemText={"Add Friend"}
+                        />
+                    </button>
+                </div>
+            </div>
+        {/* Dashboard sub-header */}
+            <div className="dashboard_expenses_summary_container">
                 <div>Total Amount: {expense?.totalAmountOwed?.toFixed(2)}</div>
+                <span>|</span>
                 <div>Amount Owed: {expense?.totalOwedAmount?.toFixed(2)}</div>
+                <span>|</span>
                 <div>User Owes: {expense?.totalOwesAmount?.toFixed(2)}</div>
             </div>
 
+            {/* Body Title Headers */}
+            <div className="dashboard_body_titles">
+                <h3>You Owe</h3>
+                <h3>You Are Owed</h3>
+            </div>
 
-            <div>
-                <h3>You Owe:</h3>
+             {/*Body  */}
+            <div className="dashboard_owe_and_owed_container">
                 <div>
-                    <div>
-                        {expense?.owesExpenses?.map(participant => (
-                        <div key={participant.id}>
-                            <span>
-                                <div>{participant.createdBy}</div>
-                                <div>{participant.amount.toFixed(2)}</div>
-                            </span>
-                            <span>
-                                {
-                                participant.amount === 0 ?    
-                                <button disabled={true}>
+                        <div className="dashboard_you_owe_container">
+                            {expense?.owesExpenses?.map(participant => (
+                            <div key={participant.id}>
+                                <div className="dashboard_body_created_amount">
+                                    <div>{participant.createdBy}</div>
+                                    <div>{participant.amount.toFixed(2)}</div>
+                                </div>
+                                <span className="dashboard_settle_details_container">
+                                    {
+                                    participant.amount === 0 ?    
+                                    <button disabled={true}>
+                                        <OpenModalMenuItem
+                                            itemText={"Settle"}
+                                            modalComponent={<SettleFormModal onSettle={() => handleSettleExpense(participant.id)} settled={participant.settled} expenseId={participant.id} amount={participant?.amount.toFixed(2)}/>}
+                                        />
+                                    </button> :
+                                    <button id="dashboard_settle_button" disabled={false}>
                                     <OpenModalMenuItem
                                         itemText={"Settle"}
                                         modalComponent={<SettleFormModal onSettle={() => handleSettleExpense(participant.id)} settled={participant.settled} expenseId={participant.id} amount={participant?.amount.toFixed(2)}/>}
                                     />
-                                </button> :
-                                 <button disabled={false}>
-                                 <OpenModalMenuItem
-                                     itemText={"Settle"}
-                                     modalComponent={<SettleFormModal onSettle={() => handleSettleExpense(participant.id)} settled={participant.settled} expenseId={participant.id} amount={participant?.amount.toFixed(2)}/>}
-                                 />
-                             </button>
-                                }
-                                <button onClick={() => navigatePaymentDuePage(participant.id)}>
-                                    Details
-                                </button>
-                            </span>
-                        </div>
-                        ))}
+                                    </button>
+                                    }
+                                    <button id="dashboard_you_owe_details_button" onClick={() => navigatePaymentDuePage(participant.id)}>
+                                        Details
+                                    </button>
+                                </span>
+                            </div>
+                            ))}
                         </div>
                 </div>
-                <div>
-                    <h3>You Are Owed:</h3>
+                <div className="divider"></div>
+                <div className="dashboard_you_are_owed_container">
                     {expense?.expensesOwed && expense?.expensesOwed.length > 0 ? 
                         expense?.expensesOwed.map(expense => (
-                            <div key={expense.id}>
+                            <div className="dashboard_you_are_owed_info_container" key={expense.id}>
                                 {expense.username.map(user => (
-                                    <div key={user.id}>  
-                                        <div>
+                                    <div className="dashboard_you_are_owed_user_amount_details" key={user.id}>  
+                                        <div className="dashboard_you_are_owed_user_expenses">
                                             <div>{user}</div>
                                             <div>
                                                 {(expense.amount/((expense.username).length)).toFixed(2)}
                                             </div>
                                         </div>
-                                        <div>
+                                        <div className="dashboard_you_are_owed_details_container">
                                             <button onClick={() => navigateAmountOwedPage(expense.id)}>
                                                 Details
                                             </button>
