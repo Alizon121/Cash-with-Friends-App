3451602ce5b6107356dcd4a132c70f74e6908917
Andrew <andrew.lizon12@gmail.com>
2025-02-12 21:02:22 -0800

    Added validations to login modal

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect, useInsertionEffect } from "react";
 import { thunkLogin } from "../../redux/session";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { useNavigate } from "react-router-dom";
+import { getUsers } from "../../redux/users";
 import "./LoginForm.css";
 
 function LoginFormModal() {
@@ -10,25 +11,46 @@ function LoginFormModal() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const users = useSelector(state => state.users.users)
   const { closeModal } = useModal();
   const navigate = useNavigate()
+
+  const emails = Object.values(users).map(user => user.email)
+
+  useEffect(() => {
+    dispatch(getUsers()) 
+  }, [dispatch])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const serverResponse = await dispatch(
-      thunkLogin({
+    // Add validations to login
+    const newErrors = {}
+    if (!emails.includes(email)) newErrors.email = "Invalid email"
+
+    if (Object.values(newErrors).length > 0) {
+      setErrors(newErrors)
+      return
+    }
+
+    try{ 
+      const response = await dispatch(thunkLogin({
         email,
         password,
       })
     );
-
-    if (serverResponse) {
-      setErrors(serverResponse);
-    } else {
+    if (response) {
       navigate("/users/dashboard")
       closeModal();
     }
+  } catch (error) {
+    if (error instanceof Error) {
+      setErrors({general: error.message});
+    } else {
+      setErrors({general: "Error: Invalid credentials"})
+    }
+  }
+    
   };
 
   return (
@@ -36,6 +58,7 @@ function LoginFormModal() {
       <div id="login_header">
         <h2>Log In</h2>
       </div>
+      {errors.general && <p className="error">{errors.general}</p>}
       <p id="sub_header_login">Sign in or create a new account</p>
       <form className="login_form_container" onSubmit={handleSubmit}>
           <label>
@@ -48,7 +71,7 @@ function LoginFormModal() {
               required
             />
           </label>
-          {errors.email && <p>{errors.email}</p>}
+          {errors.email && <p className="error">{errors.email}</p>}
           <label>
             <input
             id="login_password_input"
